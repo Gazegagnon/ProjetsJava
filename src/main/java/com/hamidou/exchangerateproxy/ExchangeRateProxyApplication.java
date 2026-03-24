0d025cdeb90417ff0a1ab029db8b515eeb1299b1
Gazegagnon <hamidoubance53@gmailcom>
2026-03-24 21:13:03 +0100

    ajout d'un scheduler automatique

--- src/main/java/com/hamidou/exchangerateproxy/ExchangeRateProxyApplication.java
+++ src/main/java/com/hamidou/exchangerateproxy/ExchangeRateProxyApplication.java
@@ -2,12 +2,16 @@ package com.hamidou.exchangerateproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Classe principale de l'application Spring Boot.
+ */
 @SpringBootApplication
+@EnableScheduling // Active l'exécution des tâches planifiées
 public class ExchangeRateProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExchangeRateProxyApplication.class, args);
     }
-
 }
